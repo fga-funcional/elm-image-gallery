cd8ecf43bc84af38dcf8b85ff955cb0164d28812
Ícaro Pires <icaropsa@gmail.com>
2018-12-04 17:54:14 -0200

    Add shortcuts for navigation

--- src/Update.elm
+++ src/Update.elm
@@ -1,12 +1,16 @@
 module Update exposing (Msg(..), init, subscriptions, update)
 
 import Array
+import Browser.Events exposing (onKeyDown)
+import Html.Events exposing (keyCode)
 import Http exposing (..)
+import Json.Decode as Decode
 import Model exposing (Image, Model, imageDecoder)
 
 
 type Msg
     = ShowSelected Int
+    | ShowCurrent
     | ShowRealSize
     | ShowResized
     | HideSelected
@@ -14,6 +18,7 @@ type Msg
     | SelectPrev
     | FetchedImg (Result Http.Error (Array.Array Image))
     | FetchImgs
+    | NoOp
 
 
 init : () -> ( Model, Cmd Msg )
@@ -26,6 +31,9 @@ update msg model =
     case msg of
         ShowSelected idx ->
             ( { model | selectedImg = idx, showBigScreen = True }, Cmd.none )
+
+        ShowCurrent ->
+            ( { model | showBigScreen = True }, Cmd.none )
 
         ShowResized ->
             ( { model | showRealSize = False }, Cmd.none )
@@ -53,10 +61,52 @@ update msg model =
                 Ok images ->
                     ( { model | imgs = images }, Cmd.none )
 
+        _ ->
+            ( model, Cmd.none )
+
 
 fixIdx : Model -> Int -> Int
 fixIdx model idx =
     modBy (Array.length model.imgs) idx
+
+
+key : Bool -> Int -> Msg
+key isDown keycode =
+    case keycode of
+        -- left
+        37 ->
+            SelectPrev
+
+        -- left - vim like
+        72 ->
+            SelectPrev
+
+        -- right
+        39 ->
+            SelectNext
+
+        -- right - vim like
+        76 ->
+            SelectNext
+
+        -- enter
+        13 ->
+            ShowCurrent
+
+        -- esc
+        27 ->
+            HideSelected
+
+        -- r
+        82 ->
+            ShowRealSize
+
+        -- f
+        70 ->
+            ShowResized
+
+        _ ->
+            NoOp
 
 
 
@@ -65,7 +115,9 @@ fixIdx model idx =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ onKeyDown (Decode.map (key True) keyCode)
+        ]
 
 
 
